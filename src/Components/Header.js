337469ac8c3bb5a34e539147c3f587db1f083f18
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-03-22 22:44:11 +0530

    Adding the header code

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
 const Header = () =>{
     return (
-    <h1 className="text-3xl font-bold underline">
-        Notes Application
-      </h1>
+<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+  <div className="container flex flex-wrap justify-between items-center mx-auto">
+    <a href="/" className="flex items-center">
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Notes Application</span>
+    </a>
+  </div>
+</nav>
     )
 }
 
